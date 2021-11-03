23f1e21fe95a3eb2088655c7334c593b9723db05
Tautvydas Shalna <tautvydasshalna@Tautvydass-MacBook-Pro.local>
2021-11-02 21:15:10 -0400

    added code

--- index.js
+++ index.js
@@ -6,13 +6,28 @@ function ask(questionText) {
     rl.question(questionText, resolve);
   });
 }
+/*-------------------------Globals---------------------*/
+function randoNumb(min,max)
+let range = max-min + 1
+return min + Math.floor(Math.random() * range) 
 
 start();
-
+/*------------------------Function Block-----------------------*/
 async function start() {
   console.log("Let's play a game where you (human) make up a number and I (computer) try to guess it.")
   let secretNumber = await ask("What is your secret number?\nI won't peek, I promise...\n");
   console.log('You entered: ' + secretNumber);
+ let compGuess = randoNumb(min,max)
+ let answer = await ask('Is your number ' +
+ compGuess + "?")
+ console.log(compGuess)
+
+
+ 
+ 
   // Now try and complete the program.
-  process.exit();
+ 
+ 
+  // process.exit();
 }
+
